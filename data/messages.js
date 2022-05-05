54d1e376cdd9312d5502846eeb0675d8d40f7ff7
ArtsyCourseExperts <96996881+ArtsyCourseExperts@users.noreply.github.com>
2022-05-05 07:55:16 -0400

    missing files

---
+++ data/messages.js
@@ -0,0 +1,40 @@
+//From https://github.com/azouaoui-med/react-pro-sidebar/blob/master/demo/src/messages.js
+//https://www.npmjs.com/package/react-intl
+
+export default {
+    ar: {
+      title: 'رياكت پرو سايد بار',
+      sidebarTitle: 'پرو سايد بار',
+      description: 'شريط جانبي مع قوائم منسدلة وعدد غير محدود من القوائم الفرعية المتداخلة',
+      dashboard: 'لوحة القيادة',
+      components: 'مكونات',
+      dropdown: 'اسقاط',
+      submenu: 'قائمة فرعية',
+      multiLevel: 'متعدد المستويات',
+      collapsed: 'انطوى',
+      rtl: 'ر ت ل',
+      image: 'صورة الخلفية',
+      new: 'جديد',
+      withPrefix: 'مع البادئة',
+      withSuffix: 'مع اللاحقة',
+      viewSource: 'عرض الكود',
+    },
+    en: {
+      title: 'React Pro Sidebar',
+      sidebarTitle: 'Pro Sidebar',
+      description:
+        'React sidebar library with dropdown menus and unlimited number of nested submenus',
+      dashboard: 'Dashboard',
+      components: 'Components',
+      dropdown: 'Dropdown',
+      submenu: 'Submenu',
+      multiLevel: 'Multi Level',
+      collapsed: 'Collapsed',
+      rtl: 'RTL',
+      image: 'Background image',
+      new: 'NEW',
+      withPrefix: 'With Prefix',
+      withSuffix: 'With Suffix',
+      viewSource: 'View Source',
+    },
+  };
